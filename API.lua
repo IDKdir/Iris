d5a0cef42de51d95097fe152311a0462ce28a344
IDKdir <121891047+IDKdir@users.noreply.github.com>
2025-04-06 11:24:14 -0400

    Update API.lua

--- API.lua
+++ API.lua
@@ -1,4 +1,11 @@
-local Types = require(script.Parent.Types)
+local owner = "IDKdir"
+local branch = "main"
+
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Iris/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+
+local Types = webImport('Types')
 
 return function(Iris: Types.Iris)
     -- basic wrapper for nearly every widget, saves space.
